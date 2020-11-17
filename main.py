310e4ff5f09e7ba9cad719839bf8aba3b708026d
yerinaaa <46762731+yerinaaa@users.noreply.github.com>
2020-11-17 20:59:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # USAGE
-# python main.py --filename video.png
-# python main.py -f video.png -e es.belyak@gmail.com -t 600 -c 90
+# python main.py --filename video.mp4
+# python main.py -f video.png -e your.mail@gmail.com -t 600 -c 90
 
 from localize import LocalizeText
 import argparse
@@ -15,10 +15,6 @@ ap.add_argument("-t", "--time_next_frame", type = int, default = 10,
 ap.add_argument("-c", "--min_conf", type = int, default = 75,
                 help = "mininum confidence value to filter weak text detection")
 args = vars(ap.parse_args())
-
-# emails = ['esbelyakova@miem.hse.ru', 'es.belyak@gmail.com']
-
-# ocr = LocalizeText(filename = 'video_test.mp4', email_to_share = emails, time_next_frame = 300, min_conf = 85)
 
 ocr = LocalizeText(filename = args["filename"], email_to_share = args["email_to_share"], time_next_frame = args["time_next_frame"], min_conf = args["min_conf"])
 
